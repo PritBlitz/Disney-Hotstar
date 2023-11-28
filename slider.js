79feddbd421c338323b2b5adcddf00f8b49072c8
Pritish Biswas <85893461+PritBlitz@users.noreply.github.com>
2023-11-28 21:47:38 +0530

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,52 @@
+let movies = [
+  {
+    name: "The Vaccine War ",
+    des: "As the COVID-19 pandemic looms over India, the medical community races to find a cure and control rumours which get more contagious than the virus.",
+    image: "images/slider 12.JPEG",
+  },
+  {
+    name: "Avengers : Endgame ",
+    des: "Surviving heroes unite to reverse Thanos' snap in an epic Marvel film. Time travel, sacrifice, and an ultimate battle shape the thrilling conclusion to this expansive cinematic saga.",
+    image: "images/slider 10.JPG",
+  },
+  {
+    name: "Koffee With Karan",
+    des: "In this celebrity talk show hosted by Karan Johar, Season 8 dives into the personal lives and controversies of Bollywood stars, showcasing candid interviews, witty banter, and behind-the-scenes glimpses.",
+    image: "images/slider 7.JPEG",
+  },
+  {
+    name: "IND vs AUS WC Finals 2023",
+    des: "In an electrifying clash, India faced Australia in the World Cup finals. Tension soared as cricket titans battled fiercely. Spectacular performances and nail-biting moments defined this epic showdown, captivating the world.",
+    image: "images/slider 6.JPEG",
+  },
+  {
+    name: "Hanuman",
+    des: " The legendary Hindu deity Hanuman embarks on new adventures, showcasing his unwavering devotion, valor, and divine prowess through epic battles that leave a lasting impact on mythology.",
+    image: "images/slider 9.JPEG",
+  },
+  {
+    name: "luca",
+    des: "Set in a beautiful seaside town on the Italian Riviera, the original animated feature is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides. ",
+    image: "images/slider 5.PNG",
+  },
+  {
+    name: "Loki",
+    des: "Embark on a cosmic odyssey in the sequel. Tom Hiddleston's magnetic performance propels the narrative into uncharted territory, unveiling unexpected alliances and mind-bending twists in this thrilling continuation. ",
+    image: "images/slider 11.JPG",
+  },
+  {
+    name: "The Night Manager (Part II)",
+    des: "Anil Kapoor returns in the riveting sequel to a suspenseful series. As the plot thickens, espionage takes center stage, promising intense drama and captivating twists in this thrilling continuation.",
+    image: "images/slider 8.JPG",
+  },
+  {
+    name: "The Falcon and The Winter Soldier",
+    des: "Sam Wilson and Bucky Barnes, post-Endgame, tackle threats and identity challenges in a six-episode Marvel series, exploring their evolving partnership in a dynamic, action-packed narrative.",
+    image: "images/slider 2.PNG",
+  },
+  {
+    name: "Wanda Vision",
+    des: "Wanda explores Wanda Maximoff's reality-bending grief after Vision's death. The Marvel series seamlessly blends sitcom homage with an intricate narrative, unraveling mysteries and expanding the MCU in nine compelling episodes.",
+    image: "images/slider 3.PNG",
+  },
+];
